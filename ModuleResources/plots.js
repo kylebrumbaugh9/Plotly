e23989eeb1c7df39d951bec22e0a3e0e18da5aa4
Kyle <kyle.brumbaugh9@gmail.com>
2023-01-22 17:54:50 -0500

    Pushing Module and Starter code

---
+++ ModuleResources/plots.js
@@ -0,0 +1,10 @@
+Plotly.newPlot("plotArea", [{x: [5, 10, 15], y: [1, 2, 3]}]);
+
+
+
+var numbers = [0,2,4,6,8];
+
+var numbersAddFive = numbers.map(function (integer) {
+    return integer + 5;
+});
+console.log(numbersAddFive);
